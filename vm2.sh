a7ff3ca188c12879fac25210ab9de8cd34749ed0
Gabriela Allende <gabriela.allende@salamancasolutions.com>
2022-05-28 01:43:25 -0400

    test

--- vm2.sh
+++ vm2.sh
@@ -4,6 +4,7 @@ docker build . -t image-subscriber:1.0.0
 TOPICO="libros"
 for i in {"${TOPICO}/fantasia","${TOPICO}/romance","${TOPICO}/suspenso","${TOPICO}/terror","${TOPICO}/comedia"}
 do
+   echo "entra"
    docker run -it --rm -env port=1883 -env topic=$i -env brokerip=research.upb.edu:21212 image-subscriber:1.0.0
 done
 
