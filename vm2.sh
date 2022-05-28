1ae8c7b28f4ad7aa030f0e234a1aadf3978827b2
Gabriela Allende <gabriela.allende@salamancasolutions.com>
2022-05-28 01:46:30 -0400

    test

--- vm2.sh
+++ vm2.sh
@@ -5,7 +5,7 @@ TOPICO="libros"
 for i in {"${TOPICO}/fantasia","${TOPICO}/romance","${TOPICO}/suspenso","${TOPICO}/terror","${TOPICO}/comedia"}
 do
    echo "entra"
-   docker run -it --rm -env port=1883 -env topic=$i -env brokerip=research.upb.edu:21212 image-subscriber:1.0.0
+   docker run -it --rm -e port=1883 -e topic=$i -e brokerip='research.upb.edu:21212' image-subscriber:1.0.0
 done
 
 
