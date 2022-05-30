295cc6cdc6edf1c46ace7b2efbc3a87495bb047f
Gabriela Allende <gabriela.allende@salamancasolutions.com>
2022-05-29 22:10:22 -0400

    test7

--- vm2.sh
+++ vm2.sh
@@ -6,6 +6,6 @@ declare -a topicos=(${TOPICO}/fantasia ${TOPICO}/romance ${TOPICO}/suspenso ${TO
 declare -a nombres=("sub1" "sub2" "sub3" "sub4" "sub5")
 for (( j=0; j<5; j++ ));
 do
-   printf "Current index %d with value %s\n" $j "${nombres[$j]}"  
+   printf "Current index %d with value %s\n" $j "${topicos[$j]}"  
  # docker run -it -d --rm -e port=21212 -e topic=${topicos[$j]} -e brokerip='research.upb.edu' --name ${nombres[$j]} image-subscriber:1.0.0 
 done
