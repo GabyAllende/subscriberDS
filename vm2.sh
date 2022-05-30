0fddd1f61574d1d4b6ba8d4714f76f0640e25147
Gabriela Allende <gabriela.allende@salamancasolutions.com>
2022-05-29 21:55:00 -0400

    test 6

--- vm2.sh
+++ vm2.sh
@@ -2,9 +2,9 @@
 cd subscriber
 docker build . -t image-subscriber:1.0.0
 TOPICO="libros"
-for i in {"${TOPICO}/fantasia","${TOPICO}/romance","${TOPICO}/suspenso","${TOPICO}/terror","${TOPICO}/comedia"}
+declare -a topicos=(${TOPICO}/fantasia ${TOPICO}/romance ${TOPICO}/suspenso ${TOPICO}/terror ${TOPICO}/comedia)
+declare -a nombres=("sub1","sub2","sub3","sub4","sub5")
+for (( j=0; j<5; j++ ));
 do
-   docker run -it -d --rm -e port=21212 -e topic=$i -e brokerip='research.upb.edu' image-subscriber:1.0.0
+  docker run -it -d --rm -e port=21212 -e topic=${topicos[$j]} -e brokerip='research.upb.edu' --name ${nombres[$j]} image-subscriber:1.0.0 
 done
-
-
