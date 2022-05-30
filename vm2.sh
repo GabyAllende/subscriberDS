1cd6ac95eeccccecc566e78af9c41f1552087496
Gabriela Allende <gabriela.allende@salamancasolutions.com>
2022-05-29 22:09:34 -0400

    test7

--- vm2.sh
+++ vm2.sh
@@ -3,7 +3,7 @@ cd subscriber
 docker build . -t image-subscriber:1.0.0
 TOPICO="libros"
 declare -a topicos=(${TOPICO}/fantasia ${TOPICO}/romance ${TOPICO}/suspenso ${TOPICO}/terror ${TOPICO}/comedia)
-declare -a nombres=("sub1","sub2","sub3","sub4","sub5")
+declare -a nombres=("sub1" "sub2" "sub3" "sub4" "sub5")
 for (( j=0; j<5; j++ ));
 do
    printf "Current index %d with value %s\n" $j "${nombres[$j]}"  
